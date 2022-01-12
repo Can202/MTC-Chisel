485c477bd0495f12e75aecda110d21b48d8da9de
Can202 <mgoopazo@gmail.com>
2022-01-12 14:29:47 -0300

    left chisel

--- scripts/chisel.lua
+++ scripts/chisel.lua
@@ -46,6 +46,46 @@ local function handler(player_name, node, itemstack, digparams)
 		minetest.check_single_for_falling(pos)
 end
 
+local function reverse_handler(player_name, node, itemstack, digparams)
+	local pos = dug_node[player_name]
+	if not pos then return end
+
+		local ndef = minetest.registered_nodes[node.name]
+		if ndef then
+			local item = ItemStack(ndef.drop or node.name)
+			local inv = minetest.get_inventory({type="player", name=player_name})
+			if inv and inv:room_for_item("main", item) then
+				local taken = inv:remove_item("main", item)
+			else
+				for _,obj in ipairs(minetest.get_objects_inside_radius(pos, 1)) do
+					obj:remove()
+					break
+				end
+			end
+		end
+		for i = 12,1,-1 do
+			local j = i-1
+
+			if j == 0 then
+				j = 12
+			end
+
+			local original = "mtc_chisel:cobblestone_" .. i
+			local next = "mtc_chisel:cobblestone_" .. j
+			if node.name == original then
+				node.name = next
+				break
+			end
+		end
+		if node.name == "default:cobble" then
+			node.name = "mtc_chisel:cobblestone_1"
+		end
+		minetest.swap_node(pos, node)
+		minetest.check_single_for_falling(pos)
+end
+
+
+
 -- Chisel
 minetest.register_tool("mtc_chisel:chisel", {
 	description = S("Chisel"),
@@ -69,4 +109,26 @@ minetest.register_craft({
 		{"","default:steel_ingot",""},
 		{"default:stick","",""},
 	}
+})
+
+-- Left-Chisel
+minetest.register_tool("mtc_chisel:left_chisel", {
+	description = S("Left Chisel"),
+	inventory_image = "mtc_chisel_left_chisel.png",
+	groups = {chisel_chisel=1},
+	tool_capabilities = {
+		groupcaps={
+			cracky = {times={[1]=2.0, [2]=1.0, [3]=0.50}, uses=30, maxlevel=3},
+		},
+	},
+	after_use = function(itemstack, user, node, digparams)
+		minetest.after(0.01, reverse_handler, user:get_player_name(), node)
+		itemstack:add_wear(digparams.wear)
+		return itemstack
+	end,
+})
+minetest.register_craft({
+	type="shapeless",
+	output = "mtc_chisel:left_chisel",
+	recipe = {"mtc_chisel:chisel","default:gold_ingot"},
 })
